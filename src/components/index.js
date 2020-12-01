5666e0d94e8a579a93820c656da7b96b9842dbc6
nivonne <nimera27@gmail.com>
2020-12-01 16:36:55 -0600

    Add Slack, Sidebar and MainContainer Components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,13 @@
 import App from './App';
 import SignIn from './SignIn';
+import Slack from './Slack';
+import Sidebar from './Sidebar';
+import MainContainer from './MainContainer';
 
 export {
     App,
-    SignIn
+    SignIn,
+    Slack, 
+    Sidebar,
+    MainContainer
 }
