75a62f44c3dc10b361a98bda85d472cef4d0032f
nivonne <nimera27@gmail.com>
2020-12-22 20:52:37 -0600

    modified some changes and fixed errors

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,7 @@
 import App from './App';
 import SignIn from './SignIn';
-import Slack from './Slack';
 import Sidebar from './Sidebar';
+import Slack from './Slack';
 import MainContainer from './MainContainer';
 
-export {
-    App,
-    SignIn,
-    Slack, 
-    Sidebar,
-    MainContainer
-}
+export { App, SignIn, Sidebar, Slack, MainContainer };
